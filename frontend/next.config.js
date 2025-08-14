911ca0a216c34d5ef5d4beb7a2ebb6937847dae6
Karim Al-Sherif <stronciy@gmail.com>
2025-08-15 01:48:56 +0300

    fix ports

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -13,6 +13,14 @@ const nextConfig = {
       },
     ]
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3110/api/:path*'
+      }
+    ]
+  },
 }
 
 module.exports = nextConfig
