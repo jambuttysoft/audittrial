bbb95ac5e876e273116c10657d24afc6628f5228
Karim Al-Sherif <stronciy@gmail.com>
2025-08-15 00:22:06 +0300

    fix headers

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,6 +3,19 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self' http: https: data: blob: 'unsafe-inline' 'unsafe-eval'; script-src 'self' 'unsafe-eval' 'unsafe-inline'; style-src 'self' 'unsafe-inline';"
+          },
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
